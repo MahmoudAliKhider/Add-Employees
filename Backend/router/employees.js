d1904dbdaf9c2a657fab5b817f94d58203bcdbbb
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-10-02 23:34:19 +0200

    Search By Id

--- Backend/router/employees.js
+++ Backend/router/employees.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const empModels = require("../models/employee")
+const objectId = require('mongoose').Types.ObjectId
 
 router.post('/add',async(req,res)=>{
     const newEmployee = new empModels(req.body)
@@ -13,7 +14,7 @@ router.post('/add',async(req,res)=>{
 })
 
 router.get("/",async(req,res)=>{
-  const Allemployee = new empModels.find()
+  const Allemployee = await empModels.find()
 
   try {
     res.status(200).json(Allemployee)
@@ -23,6 +24,24 @@ router.get("/",async(req,res)=>{
     
   }
 })
+//Search By Id
+router.get('/:id',async(req,res)=>{
+    if(objectId.isValid(req.params.id)){
+       
+            try {
+                const getuser = await empModels.findById(req.params.id)
+                res.status(200).json(getuser)
+                
+              } catch (err) {
+                res.status(400).json(err)
+                
+              }
+        
 
+     }else{
+                res.status(400).send("You can only update your profil")
+        
+          }
+})
 
 module.exports= router
